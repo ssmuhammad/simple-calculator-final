ec0bfc93abfabc448963ecb5c3084dfbd9f735fe
Salisu S. Muhammad <cyieph@gmail.com>
2026-07-04 20:50:25 +0100

    Add division and multiplication functions
    Added division and multiplication functions with print statements.

--- calc.py
+++ calc.py
@@ -2,5 +2,12 @@ def add(a, b):
   return a + b
 def subtract(a, b):
   return a - b
+def division(a, b):
+  return a/b
+def multiplication(a, b):
+  return a*b
+  
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(5, 3))
+print("Division:", division(5, 3))
+print("Multiplicaiton", multiplication(5, 3))
